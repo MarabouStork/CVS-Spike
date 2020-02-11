ecbf91cb90dc8e89f1551a4a4f3b7b89413e3ada
Simon Parsons <simon@mmaraboustork.co.uk>
2020-02-11 09:37:44 +0000

    remove endpoint from api gateway

--- cvs-spike-provision/resources_api_manager.tf
+++ cvs-spike-provision/resources_api_manager.tf
@@ -34,25 +34,6 @@ resource "aws_api_gateway_integration" "test_results" {
     uri                     = aws_lambda_function.test_results.invoke_arn
 }
 
-# # Unfortunately the proxy resource cannot match an empty path at the root of the API. To handle that, 
-# # a similar configuration must be applied to the root resource that is built in to the REST API object
-# resource "aws_api_gateway_method" "proxy_root" {
-#     rest_api_id   = aws_api_gateway_rest_api.cvs_backend.id
-#     resource_id   = aws_api_gateway_rest_api.cvs_backend.root_resource_id
-#     http_method   = "POST"
-#     authorization = "NONE"
-# }
-
-# resource "aws_api_gateway_integration" "test_results_root" {
-#     rest_api_id = aws_api_gateway_rest_api.cvs_backend.id
-#     resource_id = aws_api_gateway_method.proxy_root.resource_id
-#     http_method = aws_api_gateway_method.proxy_root.http_method
-
-#     integration_http_method = "POST"
-#     type                    = "AWS_PROXY"
-#     uri                     = aws_lambda_function.test_results.invoke_arn
-# }
-
 # Finally, you need to create an API Gateway "deployment" in order to activate the configuration and expose 
 # the API at a URL that can be used for testing
 resource "aws_api_gateway_deployment" "example" {
